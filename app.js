676a90da3ef08c9718d797f203c6d96d375fdc50
Jahmella Williams <simmons.jahmella@gmail.com>
2023-06-09 23:10:43 -0400

    Creates normal counter

---
+++ app.js
@@ -0,0 +1,32 @@
+const btns = document.querySelectorAll(".btn");
+const num = document.querySelector("#value");
+let count = 0;
+
+btns.forEach( btn => {
+    btn.addEventListener('click', function(e){
+       const counter = e.currentTarget.classList;
+
+       if(counter.contains('count-down')){
+        count--;  
+   
+       } 
+       if(counter.contains('count-up')){
+        count++;
+       } 
+       if(counter.contains('reset')){
+        count = 0;
+       }
+       if(count < 0){
+        num.style.color = 'red';
+       } else if(count > 0){
+        num.style.color = 'green';
+       } else {
+        num.style.color = 'black';
+       }
+       num.textContent =  count;
+       
+    })
+        
+    })
+
+   
