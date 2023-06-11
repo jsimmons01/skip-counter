fffda044693cb6f5473db7d7535fd8ddbf07c0ec
Jahmella Williams <simmons.jahmella@gmail.com>
2023-06-10 22:38:44 -0400

    Moves userNum to local scope

--- app.js
+++ app.js
@@ -1,46 +1,44 @@
 const btns = document.querySelectorAll(".btn");
 const num = document.querySelector("#value");
 const submitBtn = document.querySelector('#submit');
-let userNum = localStorage.getItem('input');
 
+     
 let count = 0;
 
-function getUserInput(){
-    userNum = document.querySelector('#num').value;
-    localStorage.setItem('userNum', input);
+
+submitBtn.addEventListener('click',function(e){
+    let userNum = document.getElementById('num').value;
     
-}
+    e.preventDefault();
+     console.log(userNum)
 
-submitBtn.addEventListener('click',function(){
-    console.log(getUserInput());
+     btns.forEach(btn => {
+
+        btn.addEventListener('click', function(e){
+            let counter = e.currentTarget.classList;
     
-})
-
-btns.forEach(btn => {
-
-    btn.addEventListener('click', function(e){
-        let counter = e.currentTarget.classList;
-
-        if(counter.contains('decrease')){
-            count-= getUserInput();
-        }
-        if(counter.contains('increase')){
-            count+= getUserInput();
-        }
-        if(counter.contains('reset')){
-            count = 0;
-        }
-        value.textContent = count
-        if(count< 0){
-            value.style.color = 'red';
-        }
-        if(count> 0){
-            value.style.color = 'green';    
-        }
-        if(count === 0){
-            value.style.color = 'black';
-        }    
-       
+            if(counter.contains('decrease')){
+                count-= userNum;
+            }
+            if(counter.contains('increase')){
+                count+= userNum;
+            }
+            if(counter.contains('reset')){
+                count = 0;
+            }
+            value.textContent = count
+            if(count< 0){
+                value.style.color = 'red';
+            }
+            if(count> 0){
+                value.style.color = 'green';    
+            }
+            if(count === 0){
+                value.style.color = 'black';
+            }    
+           
+        })
+        
     })
-    
 })
+
