a642ad445302572fb69d120cebf452e6f50c3ed2
Jahmella Williams <simmons.jahmella@gmail.com>
2023-06-10 21:17:02 -0400

    Creates userInput function

--- app.js
+++ app.js
@@ -1,18 +1,46 @@
 const btns = document.querySelectorAll(".btn");
 const num = document.querySelector("#value");
-const userNum = document.querySelector('#num')
+const submitBtn = document.querySelector('#submit');
+let userNum = localStorage.getItem('input');
+
 let count = 0;
+
+function getUserInput(){
+    userNum = document.querySelector('#num').value;
+    localStorage.setItem('userNum', input);
+    
+}
+
+submitBtn.addEventListener('click',function(){
+    console.log(getUserInput());
+    
+})
 
 btns.forEach(btn => {
 
     btn.addEventListener('click', function(e){
         let counter = e.currentTarget.classList;
 
-    if(counter.contains('decrease')){
-        count--;
-    }
-    console.log(count)
-    }
-    )
+        if(counter.contains('decrease')){
+            count-= getUserInput();
+        }
+        if(counter.contains('increase')){
+            count+= getUserInput();
+        }
+        if(counter.contains('reset')){
+            count = 0;
+        }
+        value.textContent = count
+        if(count< 0){
+            value.style.color = 'red';
+        }
+        if(count> 0){
+            value.style.color = 'green';    
+        }
+        if(count === 0){
+            value.style.color = 'black';
+        }    
+       
+    })
     
 })
