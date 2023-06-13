2e89d829c79aeac189ea1fd360f6b24b11e03d45
Jahmella Williams <simmons.jahmella@gmail.com>
2023-06-12 23:38:49 -0400

    Converts userNum to number

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const btns = document.querySelectorAll(".btn");
 const num = document.querySelector("#value");
-const submitBtn = document.querySelector('#submit');
-
-     
+const submitBtn = document.querySelector('#submit');    
 let count = 0;
 
 
 submitBtn.addEventListener('click',function(e){
-    let userNum = document.getElementById('num').value;
+    let userNum = Number(document.getElementById('num').value);
     
     e.preventDefault();
-     console.log(userNum)
+     console.log(userNum);
 
      btns.forEach(btn => {
 
